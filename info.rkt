55aee2deab8f575f036c2d841d98e5a9eacc7f75
Jack Firth <jackhfirth@gmail.com>
2015-08-20 23:27:58 -0700

    Remove extra info

--- info.rkt
+++ info.rkt
@@ -9,25 +9,11 @@
 
 (define deps
   '("base"
-    "fancy-app"
-    "rackunit-lib"
-    "scribble-lib"
-    "typed-racket-lib"
-    "typed-racket-more"
-    "unstable-contract-lib"))
+    "fancy-app"))
 
 
 (define build-deps
   '("cover"
     "rackunit-lib"
-    "rackunit-doc"
     "racket-doc"
     "doc-coverage"))
-
-
-(define test-omit-paths
-  '("info.rkt"
-    "request/main.scrbl"
-    "request/doc-utils"
-    "request/private/base.scrbl"
-    "request/private/struct.scrbl"))
